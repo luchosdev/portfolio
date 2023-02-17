f34c8ba2589ce985a517dbf5bc7e095fa968d764
Luis Sequera <luissequeracontreras@gmail.com>
2023-02-17 13:37:04 -0500

    add new cv

--- src/components/layouts/InputText.jsx
+++ src/components/layouts/InputText.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/prop-types */
+import React from 'react';
+
 // eslint-disable-next-line object-curly-newline
 function InputText({ placeholder, onChange, Style, name }) {
   return (
     <input
       required
-      className={`my-1 rounded-sm p-2 text-black outline-none md:text-xl lg:text-2xl ${Style}`}
+      className={`my-1 rounded-sm bg-white p-2 text-black outline-none md:text-xl lg:text-2xl ${Style}`}
       name={name}
       placeholder={placeholder}
       onChange={onChange}
